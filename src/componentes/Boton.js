298628518c04f9d1e63905b52a9bef6543320479
Karina1014 <janneth_wi15@hotmail.com>
2024-10-22 17:52:42 -0500

    realizado

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function boton(props){
+function boton({ texto }){
 
     return(
         <botton>
-            {props.texto}
+            {texto}
         </botton>
     );
 }
