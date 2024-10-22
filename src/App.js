484043105627ae2b594b14ccdfcaf33b8896e9ac
Karina1014 <janneth_wi15@hotmail.com>
2024-10-22 17:41:40 -0500

    Actualizando README.md con la descripción del proyecto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-
 import './App.css';
-import logoALline from './assets/logo.png'
+import logoALline from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <div className='logo'>
+      <div className="logo-contenedor">
         <img
-        className='logo-app'
-        src={logoALline}
-        alt='logo de Aline'
+          className="logo-app"
+          src={logoALline}
+          alt="Logo de Aline"
         />
-
+      </div>
+      <div className="contador-principal">
+        {/* Agregar la lógica para el contador */}
+        <h2>Contador: 0</h2> {/* Ejemplo de título para el contador */}
       </div>
     </div>
   );
